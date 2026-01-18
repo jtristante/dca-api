2769293fb435ed7a648d3362a4b1f18486167dba
jtristante <84666847+jtristante@users.noreply.github.com>
2026-01-18 01:09:12 +0100

    chore(dca-api): Initialize project
    Set up Maven, Spring Boot, and initial project structure

---
+++ src/test/java/dev/jtristante/dcaapi/DcaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jtristante.dcaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
